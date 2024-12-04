1532074395baa53cb68493c8e37d97618808b23b
youssefwagih <youssef.shamshon@maxab.io>
2024-12-04 10:16:21 +0200

    clean

--- src/main/java/com/example/demo/TaskRepository.java
+++ src/main/java/com/example/demo/TaskRepository.java
@@ -3,9 +3,6 @@ package com.example.demo;
 
 
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 public interface TaskRepository extends FirestoreReactiveRepository<Task> {
 }
